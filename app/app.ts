63057da4a6c768df096c0466e76e3fa3a92055ca
José Luciano <luciano.rochajl@gmail.com>
2016-10-12 19:35:25 -0300

    Pagina Gerada automaticamente

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, ionicBootstrap, MenuController } from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
-
+import { GeneratedTestPage } from './pages/generated-test/generated-test';
 
 @Component({
   templateUrl: 'build/app.html'
@@ -12,10 +12,11 @@ export class MyApp {
   pages: Array<{component: any, title: string, icon: string}>;
   rootPage: any = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private menuCtrl: MenuController) {
 
     this.pages = [
-      {component: HomePage, title: 'Home', icon: 'home'}
+      {component: HomePage, title: 'Home', icon: 'home'},
+      {component: GeneratedTestPage, title: 'Generated Page', icon: 'home'}
     ];
 
     platform.ready().then(() => {
@@ -27,7 +28,15 @@ export class MyApp {
 
   openPage(page: any) : void {
     this.rootPage = page.component;
+    this.menuCtrl.close();
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp, [], {
+  menuType: 'push',
+  platforms: {
+    ios: {
+      menuType: 'overlay',
+    }
+  }
+});
